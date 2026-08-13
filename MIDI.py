18aa55f1d2406f2a09b751b0120a897da3a4ee21
athenasinha18-creator <athenasinha18@gmail.com>
2026-08-13 15:39:44 -0700

    Create MIDI.py

---
+++ MIDI.py
@@ -0,0 +1,18 @@
+!pip install music21
+
+from music21 import note
+my_note_value= 60
+n1 = note.Note(my_note_value)
+n1.show('midi')
+superduperhigh_note_value= 72
+n2=note.Note(superduperhigh_note_value)
+n2.show("midi")
+superhigh_note_value= 84
+n3=note.Note(superhigh_note_value)
+n3.show("midi")
+super_note_value= 96
+n4=note.Note(super_note_value)
+n4.show("midi")
+super_note_value= 108
+n5=note.Note(super_note_value)
+n5.show("midi")
